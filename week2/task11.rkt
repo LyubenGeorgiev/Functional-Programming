f39f2a7ab6d1314a9acc4b79365878088c4f5cce
LyubenGeorgiev <79706202+LyubenGeorgiev@users.noreply.github.com>
2021-10-19 00:09:35 +0300

    Faster task11

--- week2/task11.rkt
+++ week2/task11.rkt
@@ -1,28 +1,15 @@
 #lang racket
 
-(define (prime? n)
-  (define (f curDiv)
-    (cond
-      [(< n (* curDiv curDiv)) #t]
-      [(zero? (remainder n curDiv)) #f]
-      [else (f (add1 curDiv))]
-      )
-    )
-  (f 2)
-  )
+(require math/number-theory)
 
 (define (nth-cubic n)
-  ; checks if a number X can be represented as Y * (Y + 1)
-  (define (special-composed number)
-    (= number (* (floor (sqrt number)) (add1 (floor (sqrt number)))))
-    )
   (define (helper index current)
-    (if (and (prime? (add1 (* index 3))) (special-composed index))
-      (if (= current n)
-        (add1 (* index 3))
-        (helper (add1 index) (add1 current))
-        )
-      (helper (add1 index) current)
+    (if (prime? (add1 (- (* 3 index index) (* 3 index))))
+        (if (= current n)
+          (add1 (- (* 3 index index) (* 3 index)))
+          (helper (add1 index) (add1 current))
+          )
+        (helper (add1 index) current)
       )
     )
   (cond
@@ -37,4 +24,6 @@
 (= (nth-cubic 50) 55897)
 (= (nth-cubic 100) 283669)
 (= (nth-cubic 200) 1570357)
+(= (nth-cubic 1000) 65524807)
+(= (nth-cubic 10000) 11712188419)
 ; (nth-cubic 0) ; should return an error
