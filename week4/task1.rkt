badca2093ce4627a3667be2ae70c3b88b486d9aa
LyubenGeorgiev <79706202+LyubenGeorgiev@users.noreply.github.com>
2021-10-27 10:21:32 +0300

    fixed name

--- week4/task1.rkt
+++ week4/task1.rkt
@@ -1,12 +1,13 @@
 #lang racket
 
-(define (g f y)
+(define (upper-bound f y)
   (λ (x) (cond
            [(< (f x) y) x]
            [else (f x)]
            ))
   )
 
-(= ((g (λ (x) (* 2 x)) 100) 50) 100)
-(= ((g (λ (x) (* 2 x)) 100.236) 500.002) 1000.004)
-(= ((g identity 1.001) 1.001) 1.001)
+
+(= ((upper-bound (lambda (x) (* x 2)) 100) 50) 100)
+(= ((upper-bound (lambda (x) (* x 2)) 100.236) 500.002) 1000.004)
+(= ((upper-bound identity 1.001) 1.001) 1.001)
