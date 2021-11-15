01fef384421e1f4a3493a207d24ccbda2c891f6f
LyubenGeorgiev <79706202+LyubenGeorgiev@users.noreply.github.com>
2021-11-15 09:09:31 +0200

    some changes

--- week4/task1.rkt
+++ week4/task1.rkt
@@ -2,12 +2,11 @@
 
 (define (upper-bound f y)
   (λ (x) (cond
-           [(< (f x) y) x]
+           [(< (f x) y) y]
            [else (f x)]
            ))
   )
 
-
-(= ((upper-bound (lambda (x) (* x 2)) 100) 50) 100)
+(= ((upper-bound (lambda (x) (* x 2)) 101) 50) 101)
 (= ((upper-bound (lambda (x) (* x 2)) 100.236) 500.002) 1000.004)
 (= ((upper-bound identity 1.001) 1.001) 1.001)
--- week6/task1.rkt
+++ week6/task1.rkt
@@ -10,3 +10,5 @@
 
 (equal? (where '(3 4 5 6 7 8 9 10) (list even? (lambda (x) (> x 5)))) '(6 8 10)) ; all even numbers greater than 5
 (equal? (where '(3 4 5 7) (list even? (lambda (x) (> x 5)))) '()) ; no numbers are even and greater than 5
+(equal? (where '() (list odd? (λ (x) (> x 3)) (λ (x) (< x 20)))) '())
+(equal? (where '(1 2 3 4 5 6 7 8 9 10 11 13 15) (list odd? (λ (x) (> x 3)) (λ (x) (< x 20)))) '(5 7 9 11 13 15))
